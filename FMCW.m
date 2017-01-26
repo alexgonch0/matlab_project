a7f8df15865bcd566e6a3ebdc8dc674ff6a5818a
Alex Goncharenko <Zigmarion@users.noreply.github.com>
2017-01-25 20:32:52 -0700

    Reverting this file back to its state from 24th Jan.

--- FMCW.m
+++ FMCW.m
@@ -1,5 +1,4 @@
-function FMCW
-
+%% General parameters
 % The following table summarizes the radar parameters.
 % 
 %  System parameters            Value
@@ -10,34 +9,27 @@ function FMCW
 %  Maximum target speed (km/h)  0
 %  Sweep time (seconds)         0.0001
          
-fc = 24e9;  %24 Ghz Wave
-c  = 3e8;   %Speed of light
+fc = 24e9;  % 24 GHz Wave
+c  = 3e8;   % Speed of light
 
-range_max_meters   = 4;        %Bottom of tank in rail car
-sweep_time_seconds = 0.0001;   %Use sweep of 0.1ms
-range_res_meters   = 0.01;     %1 cm resolution
+range_max_meters   = 4;        % Bottom of tank in rail car
+sweep_time_seconds = 0.0001;   % Use sweep of 0.1ms
+range_res_meters   = 0.01;     % 1 cm resolution
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Determine Sweep bandwidth
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-lambda = c/fc;  %wavelength
-bw = range2bw(range_res_meters,c); %required bandwidth 
+lambda = c/fc;                       % wavelength
+bw = range2bw(range_res_meters,c);   % required bandwidth 
 sweep_slope = bw/sweep_time_seconds; 
 
-%maximum beat frequency can be calcualted
+% Maximum beat frequency
 fr_max = range2beat(range_max_meters,sweep_slope,c);
 fb_max = fr_max;
 
-
-%%
-% This example adopts a sample rate of the larger of twice the maximum beat
-% frequency and the bandwidth.
-
+% We use sample rate of the larger of twice the maximum beat frequency and 
+% the bandwidth
 fs = max(2*fb_max,bw);
 
 
-%%
-%Setup the radar FMCW wave
+%% FMCW wave
 waveform = phased.FMCWWaveform('SweepTime',sweep_time_seconds,'SweepBandwidth',bw,...
     'SampleRate',fs);
 
@@ -47,18 +39,21 @@ spectrogram(sig,32,16,32,fs,'yaxis');
 title('FMCW signal spectrogram');
 
 %% Target Model
-car_bottom = 2;
-car_rcs = db2pow(min(10*log10(car_bottom)+5,20));
+c1 = 1.9768e8;
 
-cartarget = phased.RadarTarget('Model','Nonfluctuating','MeanRCS',car_rcs,'PropagationSpeed',c,...
-    'OperatingFrequency',fc);
+distance_comm = 2;  % distance between the radar and commodity surface
+rcs_comm = db2pow(min(10*log10(distance_comm)+5,20));   % cross-section of the commodity under
+                                                        % the radar
 
-%cartarget = phased.RadarTarget('Model','Swerling2','MeanRCS',car_rcs,'PropagationSpeed',c,...
-%   'OperatingFrequency',fc);
+target_comm = phased.RadarTarget('Model','Nonfluctuating','MeanRCS',rcs_comm,...
+    'PropagationSpeed',c1,'OperatingFrequency',fc);
 
-carmotion = phased.Platform();
+%cartarget = phased.RadarTarget('Model','Swerling2','MeanRCS',car_rcs,...
+%   'PropagationSpeed',c,'OperatingFrequency',fc);
 
-%%
+motion_comm = phased.Platform();
+
+%% Channel
 % The propagation model is assumed to be free space.
 
 channel = phased.FreeSpace('PropagationSpeed',c,...
@@ -66,94 +61,86 @@ channel = phased.FreeSpace('PropagationSpeed',c,...
 
 %% Radar System Setup
 % The rest of the radar system includes the transmitter, the receiver, and
-% the antenna. This example uses the parameters presented in [1]. Note that
-% this example models only main components and omits the effect from other
-% components, such as coupler and mixer. In addition, for the sake of
-% simplicity, the antenna is assumed to be isotropic and the gain of the
-% antenna is included in the transmitter and the receiver.
+% the antenna. Currently the antenna is assumed to be isotropic and the
+% gain of the antenna is included in the transmitter and the receiver.
 
 ant_aperture = 6.06e-4;                         % in square meter
 ant_gain = aperture2gain(ant_aperture,lambda);  % in dB
 
 tx_ppower = db2pow(1)*1e-3;                     % in watts
-tx_gain = 9+ant_gain;                           % in dB
+tx_gain = 9 + ant_gain;                         % in dB
 
-rx_gain = 15+ant_gain;                          % in dB
+rx_gain = 15 + ant_gain;                        % in dB
 rx_nf = 4.5;                                    % in dB
 
 transmitter = phased.Transmitter('PeakPower',tx_ppower,'Gain',tx_gain);
 receiver = phased.ReceiverPreamp('Gain',rx_gain,'NoiseFigure',rx_nf,...
     'SampleRate',fs);
 
-%%
+%% Radar Motion
 % Automotive radars are generally mounted on vehicles, so they are often in
 % motion. This example assumes the radar is traveling at a speed of 100
 % km/h along x-axis. So the target car is approaching the radar at a
 % relative speed of 4 km/h.
 
-radarmotion = phased.Platform();
+motion_radar = phased.Platform();
 
 %% Radar Signal Simulation
 % # The waveform generator generates the FMCW signal.
 % # The transmitter and the antenna amplify the signal and radiate the
-% signal into space.
+%   signal into space.
 % # The signal propagates to the target, gets reflected by the target, and
-% travels back to the radar.
+%   travels back to the radar.
 % # The receiving antenna collects the signal.
 % # The received signal is dechirped and saved in a buffer.
 % # Once a certain number of sweeps fill the buffer, the Fourier transform
-% is performed in both range and Doppler to extract the beat frequency as
-% well as the Doppler shift. One can then estimate the range and speed of
-% the target using these results. Range and Doppler can also be shown as an
-% image and give an intuitive indication of where the target is in the
-% range and speed domain.
+%   is performed in both range and Doppler to extract the beat frequency as
+%   well as the Doppler shift. One can then estimate the range and speed of
+%   the target using these results. Range and Doppler can also be shown as
+%   an image and give an intuitive indication of where the target is in the
+%   range and speed domain.
 
+Nsweep = 4;
+for m = 1:Nsweep
+    % Update radar and target positions
+    [radar_pos,radar_vel] = motion_radar(waveform.SweepTime);
+    [tgt_pos,tgt_vel] = motion_comm(waveform.SweepTime);
 
+    % Transmit FMCW waveform
+    sig = waveform();
 
-        Nsweep = 4;
-        % Alex comment
-        % #justcapstonethings
+    sig = phase_noise(sig);
 
-        for m = 1:Nsweep
-            % Update radar and target positions
-            [radar_pos,radar_vel] = radarmotion(waveform.SweepTime);
-            [tgt_pos,tgt_vel] = carmotion(waveform.SweepTime);
+    txsig_init = transmitter(sig);
 
-            % Transmit FMCW waveform
-            sig = waveform();
-            
-            %% Phase Noise
-            %sig = phase_noise(sig);
-            txsig_init = transmitter(sig);
+    % Propagate the signal and reflect off the target
+    tgt_pos(1) = 2;
+    txsig = channel(txsig_init,radar_pos,tgt_pos,radar_vel,tgt_vel);
+    % UPDATE = false; %only used for Swirl1 stuff
 
-            %% Propagate the signal and reflect off the target
-            tgt_pos(1) = 2;
-            txsig = channel(txsig_init,radar_pos,tgt_pos,radar_vel,tgt_vel);
-            % UPDATE = false; %only used for Swirl1 stuff
-            % txsig = cartarget(txsig,UPDATE); %step(H,X,UPDATE)
+    % txsig = cartarget(txsig,UPDATE); %step(H,X,UPDATE)
+    txsig = target_comm(txsig); %step(H,X,UPDATE)
 
-            txsig = cartarget(txsig); %step(H,X,UPDATE)
+    % Add circulator coupling
+    % Probably should actually be here -- Alex
+    txsig = circulator(0.25, txsig_init, txsig);
+    
+    % Dechirp the received radar return
+    txsig = receiver(txsig);
 
-            %%  Dechirp the received radar return
-            txsig = receiver(txsig);
-            
-            %%  Mix original signal with received one conjugated:
-            %txsig = circulator(5, txsig_init, txsig);
-            txsig = (txsig + (0.5.*sig));
-            dechirpsig = dechirp(txsig,sig);
+    dechirpsig = dechirp(txsig,sig);
 
-            %%  FFT and range of object
-            FFT_range(c,fs,dechirpsig,sweep_slope)
-            
-            figure(2)
-            spectrogram(sig,32,16,32,fs,'yaxis');
-        end
+    % FFT and range of object
+    FFT_range(c,fs,dechirpsig,sweep_slope)
 
+    figure(2)
+    spectrogram(sig,32,16,32,fs,'yaxis');
 end
 
 
+
 function FFT_range (c,Fs,IQ_data,sweep_slope)
-    %FFT
+    % FFT
     T = 1/Fs;             % Sampling period
     L = length(IQ_data);  % Length of signal
     t = (0:L-1)*T;        % Time vector
@@ -164,32 +151,32 @@ function FFT_range (c,Fs,IQ_data,sweep_slope)
     P1 = P2(1:L/2+1);
     P1(2:end-1) = 2*P1(2:end-1);
     
-    rng = c*f/sweep_slope/2; %  RANGE PLOT IN M 
+    rng = c*f/sweep_slope/2; % RANGE PLOT IN M 
     figure(3)
     hold on
-    axis([0 6 -170 10])
+    axis([0 6 -100 0])
     plot(rng,mag2db(P1))
     title('Range Power Plot')
     xlabel('Range (m)')
     ylabel('|P1 db(m)|')
 
     
-    %Est Range
-    [y,x] = max(mag2db(P1)) % find peak FFT point
+    % Range estimation
+    [y,x] = max(mag2db(P1)); % find peak FFT point
     rng(x) % map to range array
 end
 
-function [txsig_out] = circulator(coupling_factor, initial, target)
-    txsig_out = target + coupling_factor * initial;
+function [txsig_out] = circulator(coupling_factor, tx_signal, rx_signal)
+    txsig_out = rx_signal + coupling_factor * tx_signal;
 end
 
 function [IQ_Data] = phase_noise(IQ_Data)
-    pnoise = comm.PhaseNoise('Level',-80,'FrequencyOffset',100000);
-    IQ_Data = pnoise(IQ_Data);
-    %IQ_Data = awgn(IQ_Data,40,'measured','db');
+    %pnoise = comm.PhaseNoise('Level',-40,'FrequencyOffset',10);
+    %IQ_Data = pnoise(IQ_Data);
+    IQ_Data = awgn(IQ_Data,30,'measured','db');
 end
 
-function dechirped_output = Mixer(Transmit_Waveform, Received_Waveform)
+function dechirped_output = mixer(Transmit_Waveform, Received_Waveform)
 % This is a stand-alone mixer function. It's not yet complete, I'm still
 % working on it, but if you see anything that can be fixed or if you have
 % any suggestions to improve it, just let me know.
@@ -212,7 +199,9 @@ plot(Received_Waveform)
 
 x = step(Received_Waveform);
 
-dechirped_output = dechirp(xref, x);
+dechirped_output = dechirp(x, xref);
+
+phase_noise(dechirped_output);
 
 figure()
 plot(real(dechirped_output))
@@ -221,6 +210,66 @@ plot(real(dechirped_output))
 [Pxx,F] = periodogram(dechirped_output,[],1024,Fs,'centered');
 figure()
 plot(F/1000,10*log10(Pxx)); grid;
+xlabel('Frequency (kHz)');
+ylabel('Power/Frequency (dB/Hz)');
+title('Periodogram Power Spectral Density Estimate After Dechirping');
+end
+
+function for_loop_mixer()
+for m = 1:Nsweep
+    % Update radar and target positions
+    [radar_pos,radar_vel] = radarmotion(waveform.SweepTime);
+    [tgt_pos,tgt_vel] = carmotion(waveform.SweepTime);
+
+    % Transmit FMCW waveform
+    sig = waveform();
+    pnoise = comm.PhaseNoise('Level',-60,'FrequencyOffset',20);
+    txsig = transmitter(sig);
+    
+    % Propagate the signal and reflect off the target
+    txsig = channel(txsig,radar_pos,tgt_pos,radar_vel,tgt_vel);
+    txsig = cartarget(txsig);
+    
+    if m == 4
+        figure();
+        plot(real(txsig));
+        xlabel('Amplitude (v)');
+        ylabel('Time (ms)');
+        title('Received Signal');
+    
+        figure();
+        plot(real(sig));
+        xlabel('Amplitude (v)');
+        ylabel('Time (ms)');
+        title('Transmitted Signal');
+    end
+    
+    % Dechirp the received radar return
+    txsig = receiver(txsig);    
+    dechirpsig = dechirp(txsig,sig);
+    
+    pnoise = comm.PhaseNoise('Level',-60,'FrequencyOffset',20);
+    pnoise(dechirpsig)
+    
+    if Nsweep == 4
+        figure();
+        plot(real(dechirpsig));
+        xlabel('Amplitude (v)');
+        ylabel('Time (ms)');
+        title('Mixed Signal');
+    end
+     
+    % Visualize the spectrum
+    specanalyzer([txsig dechirpsig]);
+    
+    xr(:,m) = dechirpsig;
+    
+end 
+
+figure()
+hold on
+[Pyy,F] = periodogram(dechirpsig,[],2048,Fs,'centered');
+plot(F/1000,10*log10(Pyy));
 xlabel('Frequency (kHz)');
 ylabel('Power/Frequency (dB/Hz)');
 title('Periodogram Power Spectral Density Estimate After Dechirping');
