b64412e35b8dfd08013e2c0f652a53e14895b1be
eramirez02 <eramirez@ucalgary.ca>
2017-01-26 20:23:29 -0700

    more comments

--- SFCW_2011.m
+++ SFCW_2011.m
@@ -3,21 +3,22 @@ function SFCW
 %% User Entry Here
 
 
-fc = 24e9;      %24 Ghz Wave
-c = 3e8;        %Speed of light
-lambda = c/fc;  %wavelength
-Nsweep = 1;     %Perform N sweeps of the radar and overlap the plots
+fc = 24e9;       %24 Ghz is the system operating frequency
+c = 3e8;         %Speed of light 
+Nsweep = 1;      %Number of sweep for the radar to perform with the radar (overlap the plots)
 
-BW = 2e9        %2Ghz BW
-Fc = 4e6        %Minimum Freq ex 2Mhz so we hvae 1000 steps
-tot_sweep_time  = 4e-3  % (s) long sweep times create large signal arrays (slow)
+BW = 2e9;        %System Bandwidth (higer bandwidth provide better resolution for target range
+Fc = 4e6;        %Minimum frequency which dictates the number of steps ex 2Mhz so we hvae 1000 steps
+                    %which also increases the range resolution 
+                    
+tot_sweep_time  = 1e-3;  % (s) long sweep times create large signal arrays (slow) 
 
-Phase_NoiseAndOffset    = [-90,100e3] %Noise and Offset 
-SystemWhite_Noise       = [-60]       %Iq Noise floor
-Circulator_Issolation   = [-40];      %-20dB issolation
+Phase_NoiseAndOffset    = [-80,100e3]; %Noise and Offset 
+SystemWhite_Noise       = [-60];       %Iq Noise floor NOT USED IN THIS VERSION
+Circulator_Issolation   = [-30];      %Issolation in TX RX circulator coupling
 
-distance_comm   = 4;    % (m) distance between the radar and commodity surface
-comm_perm       = 2.3;  % (e) Commodity permitivity
+distance_comm   = 1.5;    % (m) distance between the radar and commodity surface
+comm_perm       = 2.3;    % (e) Commodity permitivity
 
 %  End User Entry                     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -31,30 +32,24 @@ comm_perm       = 2.3;  % (e) Commodity permitivity
 
 
 %% Begin Test Code
-FreqSteps = BW/Fc 
-fs  = BW*2;
-tot_points = fs*tot_sweep_time
-points_per_step = tot_points/FreqSteps
+lambda = c/fc;          %wavelength
+FreqSteps = BW/Fc;       %calculate number of steps
+fs  = BW*2;             %Samplign frequency at 4Ghz
+tot_points = fs*tot_sweep_time; %points for given sweep time
+points_per_step = tot_points/FreqSteps;
 L = points_per_step;
 wave = zeros(L,FreqSteps);
 figure(1)
 
 
 
-
+%create a sine wave for every step
 for steps = 1:FreqSteps
    t = [0:1:points_per_step-1];
    I  = cos(((2*pi*(Fc*steps/(2*BW)*t))));
    Q  = sin(((2*pi*(Fc*steps/(2*BW)*t))));
    Z  = I - 1i*Q;
-   %wave = vertcat(wave,Z');
-   %Z_Phase = phase_noise(Z',Phase_NoiseAndOffset(1),Phase_NoiseAndOffset(2));
-   %wave(:,steps) = Z_Phase;
    wave(:,steps) = Z';
-   %plot(I)
-   %Frq = Fc*steps;
-   %steps
-   %plot(real(wave(:,steps)));
 end
 steps
 
@@ -65,13 +60,9 @@ c1 = 1/ sqrt((4*pi*10e-7)*(8.854*10e-12)*(comm_perm)); %Propagation speed calcul
 target_comm = phased.RadarTarget('Model','Nonfluctuating','MeanRCS',rcs_comm,'PropagationSpeed',c1,...
     'OperatingFrequency',fc);
 
-%target_comm = phased.RadarTarget('Model','Swerling2','MeanRCS',rcs_comm,'PropagationSpeed',c,...
-%   'OperatingFrequency',fc);
-
 
 %%
 % The propagation model is assumed to be free space.
-
 channel = phased.WidebandFreeSpace('PropagationSpeed',c,...
     'OperatingFrequency',fc,'SampleRate',fs,'TwoWayPropagation',true);
 
@@ -102,53 +93,44 @@ radarmotion = phased.Platform('InitialPosition',[0;0;0]);
 
 
 %% Actual Sweep
-
 sig_combined = combineSteps(wave,FreqSteps); %Combine all steps into one wavefform
 
 
-for m = 1:Nsweep
-    %% Add any phase noise
-    sig = phase_noise(sig_combined,Phase_NoiseAndOffset(1),Phase_NoiseAndOffset(2));
-    plotSweepSpectrum(sig,fs); %Plot the Spectrogram
-    disp('Sweeping')
-    Nsweep
-    
-    %% Setup the TX signal
-    %txsig =  transmitter(sig); MATLAB2016b syntax
-     txsig = step(transmitter,sig);
-     
-    %% Propagate the signal and reflect off the target
-    %[radar_pos,radar_vel] = radarmotion(1);
-    [radar_pos,radar_vel] = step(radarmotion,(1));
-    
-    %txsig = channel(txsig,radar_pos,tgt_pos,radar_vel,radar_vel);
-    txsig = step(channel,txsig,radar_pos,tgt_pos,radar_vel,radar_vel);
-    
-    %txsig = target_comm(txsig); %step(H,X,UPDATE)
-     txsig = step(target_comm,txsig); %step(H,X,UPDATE)
-    
-    %% Dechirp the received radar return
-    %txsig = receiver(txsig);    
-    txsig = step(receiver,txsig);
-    
-    
-    %% Add Coupling dechirp and LPF
-    txsig = circulator(Circulator_Issolation,sig,txsig);
-    dechirpsig       = dechirp(txsig,sig);
-    dechirpsig       = IQ_filter(dechirpsig); %Fillter the data through a LPF
+        for m = 1:Nsweep
+        %% Add any phase noise
+        sig = phase_noise(sig_combined,Phase_NoiseAndOffset(1),Phase_NoiseAndOffset(2));
+        plotSweepSpectrum(sig,fs); %Plot the Spectrogram
+        disp('Sweeping')
+        Nsweep
 
-    %% Plot FFT
-    FFT_range(c,fs,dechirpsig,FreqSteps,BW,tot_sweep_time)
-   
+        %% Setup the TX signal
+         txsig = step(transmitter,sig);
 
-    end
+        %% Propagate the signal and reflect off the target
+        [radar_pos,radar_vel] = step(radarmotion,(1));
+
+        txsig = step(channel,txsig,radar_pos,tgt_pos,radar_vel,radar_vel);
+
+        txsig = step(target_comm,txsig); %step(H,X,UPDATE)
+
+        %% Dechirp the received radar return
+        txsig = step(receiver,txsig);
+
+
+        %% Add Coupling, dechirp and LPF
+        txsig = circulator(Circulator_Issolation,sig,txsig);
+        dechirpsig       = dechirp(txsig,sig);
+        dechirpsig       = IQ_filter(dechirpsig); %Fillter the data through a LPF
+
+        %% Plot FFT
+        FFT_range(c,fs,dechirpsig,FreqSteps,BW,tot_sweep_time)
+        end
 end   
 
 
  
-%% FilterDesigner LPF for filter IQ jumps
-
- function [filtered_data] = IQ_filter(IQ_data)
+%% FilterDesigner LPF for filtering IQ jumps
+function [filtered_data] = IQ_filter(IQ_data)
 % All frequency values are in Hz.
 Fs = 4000000000;  % Sampling Frequency
 
@@ -188,11 +170,11 @@ end
     dR = sweeptime*(speedOfLight/2);
     dR = dR/L;
     deltaR = linspace(dR,dR*L,L/2+1); %frequency to range conversion
-    deltaR = deltaR - (deltaR(1)); %fixes distance offset - 0Hz = 0m
+    deltaR = deltaR - (deltaR(1));    %fixes distance offset - 0Hz = 0m
     
     figure(3)
     hold on
-    axis([0 10 -100 -10])
+    axis([0 10 -100 0])
     plot(deltaR,mag2db(P1))
     title('Range Power Plot')
     xlabel('Range (m)')
@@ -200,6 +182,7 @@ end
     title('FFT Object Range and Magnitude');
     %Est Range
     [y,x] = max(mag2db(P1)); % find peak FFT point
+    disp('Distance of object based on FFT (m):')
     deltaR(x)
  end
 
@@ -221,18 +204,10 @@ combined = wholesig;
  title('SFCW Signal Spectrogram/Sweep-time');
  end
   
- %% Estimate Range
- function [res] = range_resolution(c,steps,BW,fs)
- L = length(dechirpsig);  % Length of signal
- f = fs*(0:(L/2))/L;
- deltaR = c ./ (2*f);
- end
-
  %% Adding IQ phasenoise
  function [IQ_Data_noise] = phase_noise(IQ_Data,PhaseNoise,Offset)
  pnoise = comm.PhaseNoise('Level',PhaseNoise,'FrequencyOffset',Offset, ...
      'SampleRate',2*Offset);
- %IQ_Data_noise = pnoise(IQ_Data); %2016B MATLAB only
  IQ_Data_noise = step(pnoise,IQ_Data);
  WhiteNoise    = awgn(IQ_Data_noise,1,.01);
  IQ_Data_noise = WhiteNoise;
