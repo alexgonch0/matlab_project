7ce9265f506635d6c3658c08250377c316061e4c
eramirez02 <eramirez@ucalgary.ca>
2017-01-26 22:12:55 -0700

    SFCW like var

--- FMCW_2011.m
+++ FMCW_2011.m
@@ -1,26 +1,43 @@
 
+function FMCW_2011
+%% FMCW Radar Signal Simulation
+% # The waveform generator generates the FMCW signal.
+% # The transmitter and the antenna amplify the signal and radiate the
+%   signal into space.
+% # The signal propagates to the target, gets reflected by the target, and
+%   travels back to the radar.
+% # The receiving antenna collects the signal.
+% # The received signal is dechirped and saved in a buffer.
+% # FFT of beat freq and range plotted 
 
-%% General parameters
-% The following table summarizes the radar parameters.
-% 
-%  System parameters            Value
-%  ----------------------------------
-%  Operating frequency (GHz)    24
-%  Maximum target range (m)     4
-%  Range resolution (m)         0.01
-%  Maximum target speed (km/h)  0
-%  Sweep time (seconds)         0.0001
-         
-fc = 24e9;  % 24 GHz Wave
-c  = 3e8;   % Speed of light
 
-range_max_meters   = 4;        % Bottom of tank in rail car
-sweep_time_seconds = 0.0001;   % Use sweep of 0.1ms
-range_res_meters   = 0.01;     % 1 cm resolution
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% User Entry Here
+fc = 24e9;       %24 Ghz is the system operating frequency
+c = 3e8;         %Speed of light 
+Nsweep = 1;
+
+range_max_meters   = 4;       % Bottom of tank in rail car
+tot_sweep_time     = 1e-3;    % Use sweep of 1ms (long sweeps create large arrays at high range resolution
+range_res_meters   = 0.05;    % 5 cm resolution
+
+Phase_NoiseAndOffset    = [-80,100e3]; %Noise and Offset 
+SystemWhite_Noise       = -60;       %Iq Noise floor NOT USED IN THIS VERSION
+Circulator_Issolation   = -30;       %Issolation in TX RX circulator coupling
+
+distance_comm   = 1.5;    % (m) distance between the radar and commodity surface
+comm_perm       = 2.3;    % (e) Commodity permitivity
+%  End User Entry                     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+%% Start Sweep Code here:
 
 lambda = c/fc;                       % wavelength
-bw = range2bw(range_res_meters,c);   % required bandwidth 
-sweep_slope = bw/sweep_time_seconds; 
+bw = range2bw(range_res_meters,c);   % bandwidth 
+sweep_slope = bw/tot_sweep_time;     
 
 % Maximum beat frequency
 fr_max = range2beat(range_max_meters,sweep_slope,c);
@@ -32,28 +49,16 @@ fs = max(2*fb_max,bw);
 
 
 %% FMCW wave
-waveform = phased.FMCWWaveform('SweepTime',sweep_time_seconds,'SweepBandwidth',bw,...
+waveform = phased.FMCWWaveform('SweepTime',tot_sweep_time,'SweepBandwidth',bw,...
     'SampleRate',fs);
+sig = step(waveform);
 
-sig = waveform();
-figure(1)
-spectrogram(sig,32,16,32,fs,'yaxis');
-title('FMCW signal spectrogram');
 
 %% Target Model
-c1 = 1.9768e8;
-
-distance_comm = 2;  % distance between the radar and commodity surface
-rcs_comm = db2pow(min(10*log10(distance_comm)+5,20));   % cross-section of the commodity under
-                                                        % the radar
-
+c1 = 1/ sqrt((4*pi*10e-7)*(8.854*10e-12)*(comm_perm));  %Propagation speed calculation
+rcs_comm = db2pow(min(10*log10(distance_comm)+5,20));   % cross-section of the commodity under the radar
 target_comm = phased.RadarTarget('Model','Nonfluctuating','MeanRCS',rcs_comm,...
     'PropagationSpeed',c1,'OperatingFrequency',fc);
-
-%cartarget = phased.RadarTarget('Model','Swerling2','MeanRCS',car_rcs,...
-%   'PropagationSpeed',c,'OperatingFrequency',fc);
-
-motion_comm = phased.Platform();
 
 %% Channel
 % The propagation model is assumed to be free space.
@@ -69,78 +74,60 @@ channel = phased.FreeSpace('PropagationSpeed',c,...
 ant_aperture = 6.06e-4;                         % in square meter
 ant_gain = aperture2gain(ant_aperture,lambda);  % in dB
 
-tx_ppower = db2pow(1)*1e-3;                     % in watts
-tx_gain = 9 + ant_gain;                         % in dB
+tx_power = db2pow(ant_gain)*db2pow(1)*1e-3;     % in watts
+tx_gain  = 9+ant_gain;                          % in dB
 
-rx_gain = 15 + ant_gain;                        % in dB
-rx_nf = 4.5;                                    % in dB
+rx_gain = 15+ant_gain;                          % RX LNA gain in dB
+rx_nf   = 3;                                    % Noise Figure in dB
 
-transmitter = phased.Transmitter('PeakPower',tx_ppower,'Gain',tx_gain);
+tgt_pos = [distance_comm;0;0];                  % x y z position of target object.
+
+transmitter = phased.Transmitter('PeakPower',tx_power,'Gain',tx_gain);
 receiver = phased.ReceiverPreamp('Gain',rx_gain,'NoiseFigure',rx_nf,...
     'SampleRate',fs);
+radarmotion = phased.Platform('InitialPosition',[0;0;0]);
 
-%% Radar Motion
-% Automotive radars are generally mounted on vehicles, so they are often in
-% motion. This example assumes the radar is traveling at a speed of 100
-% km/h along x-axis. So the target car is approaching the radar at a
-% relative speed of 4 km/h.
 
-motion_radar = phased.Platform();
 
-%% Radar Signal Simulation
-% # The waveform generator generates the FMCW signal.
-% # The transmitter and the antenna amplify the signal and radiate the
-%   signal into space.
-% # The signal propagates to the target, gets reflected by the target, and
-%   travels back to the radar.
-% # The receiving antenna collects the signal.
-% # The received signal is dechirped and saved in a buffer.
-% # Once a certain number of sweeps fill the buffer, the Fourier transform
-%   is performed in both range and Doppler to extract the beat frequency as
-%   well as the Doppler shift. One can then estimate the range and speed of
-%   the target using these results. Range and Doppler can also be shown as
-%   an image and give an intuitive indication of where the target is in the
-%   range and speed domain.
+        for m = 1:Nsweep
+            %% Propagate the signal and reflect off the target
+            [radar_pos,radar_vel] = step(radarmotion,(1));
+            
+            %% Add any phase noise
+            sig = phase_noise(sig,Phase_NoiseAndOffset(1),Phase_NoiseAndOffset(2));
+            plotSweepSpectrum(sig,fs); %Plot the Spectrogram
+            disp('Sweeping')
+            Nsweep
 
-Nsweep = 4;
-for m = 1:Nsweep
-    % Update radar and target positions
-    [radar_pos,radar_vel] = motion_radar(waveform.SweepTime);
-    [tgt_pos,tgt_vel] = motion_comm(waveform.SweepTime);
+            
+            %% Setup the TX signal
+            txsig = step(transmitter,sig);
 
-    % Transmit FMCW waveform
-    sig = waveform();
 
-    sig = phase_noise(sig);
+            %% Propagate the signal and reflect off the target
+            txsig = step(channel,txsig,radar_pos,tgt_pos,radar_vel,radar_vel);
+            txsig = step(target_comm,txsig); 
 
-    txsig_init = transmitter(sig);
 
-    % Propagate the signal and reflect off the target
-    tgt_pos(1) = 2;
-    txsig = channel(txsig_init,radar_pos,tgt_pos,radar_vel,tgt_vel);
-    % UPDATE = false; %only used for Swirl1 stuff
+            %% Add Coupling, dechirp and LPF
+            txsig = circulator(Circulator_Issolation,sig,txsig);
+        
 
-    % txsig = cartarget(txsig,UPDATE); %step(H,X,UPDATE)
-    txsig = target_comm(txsig); %step(H,X,UPDATE)
+            %% Received radar return
+            txsig = step(receiver,txsig);
 
-    % Add circulator coupling
-    % Probably should actually be here -- Alex
-    txsig = circulator(0.20, txsig_init, txsig);
-    
-    % Dechirp the received radar return
-    txsig = receiver(txsig);
 
-    dechirpsig = dechirp(txsig,sig);
+            %% Dechirp 
+            dechirpsig       = dechirp(txsig,sig);
 
-    % FFT and range of object
-    FFT_range(c,fs,dechirpsig,sweep_slope)
+            %% FFT Plot
+            FFT_range(c,fs,dechirpsig,sweep_slope)
 
-    figure(2)
-    spectrogram(sig,32,16,32,fs,'yaxis');
+
+        end
 end
 
-
-
+%% FFT Plotting and range
 function FFT_range (c,Fs,IQ_data,sweep_slope)
     % FFT
     T = 1/Fs;             % Sampling period
@@ -156,30 +143,42 @@ function FFT_range (c,Fs,IQ_data,sweep_slope)
     rng = c*f/sweep_slope/2; % RANGE PLOT IN M 
     figure(3)
     hold on
-    axis([0 6 -100 0])
+    axis([0 10 -100 10])
     plot(rng,mag2db(P1))
     title('Range Power Plot')
     xlabel('Range (m)')
     ylabel('|P1 db(m)|')
-
+    title('FMCW FFT Object Range and Magnitude');
     
     % Range estimation
     [y,x] = max(mag2db(P1)); % find peak FFT point
+    disp('Distance of object based on FFT (m):')
     rng(x) % map to range array
 end
 
-function [txsig_out] = circulator(coupling_factor, tx_signal, rx_signal)
-    txsig_out = rx_signal + coupling_factor * tx_signal;
-end
+ %% Adding Circulator Coupling
+ function [txsig_out] = circulator(coupling_factor, initial, target)
+    coupling_factor = 10^(coupling_factor/10);	
+    txsig_out = target + coupling_factor * initial;
+ end
 
-function [IQ_Data] = phase_noise(IQ_Data) 
-    %pnoise = comm.PhaseNoise('Level',-40,'FrequencyOffset',10);
-    %IQ_Data = pnoise(IQ_Data);
-    
-    % PhaseNoise doesn't work as expected, so awgn is used for now
-    IQ_Data = awgn(IQ_Data,30,'measured','db');
-end
 
+ %% Adding IQ phasenoise
+ function [IQ_Data_noise] = phase_noise(IQ_Data,PhaseNoise,Offset)
+ pnoise = comm.PhaseNoise('Level',PhaseNoise,'FrequencyOffset',Offset, ...
+     'SampleRate',2*Offset);
+ IQ_Data_noise = step(pnoise,IQ_Data);
+ WhiteNoise    = awgn(IQ_Data_noise,1,.01);
+ IQ_Data_noise = WhiteNoise;
+ end
+ 
+ %% Plotting Spectrogram
+ function plotSweepSpectrum(data,fs)
+ figure(1)
+ spectrogram(data,32,16,32,fs,'yaxis');
+ title('FMCW Signal Spectrogram/Sweep-time');
+ end
+  
 function dechirped_output = mixer(Transmit_Waveform, Received_Waveform)
 % This is a stand-alone mixer function. It's not yet complete, I'm still
 % working on it, but if you see anything that can be fixed or if you have
